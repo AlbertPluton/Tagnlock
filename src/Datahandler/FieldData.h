3611e560f538df6e5cb3c11185b283fb50ce49c1
bart <bart@pluton.nl>
2008-12-17 12:25:09 +0000

    Worked on the FieldData

--- src/Datahandler/FieldData.h
+++ src/Datahandler/FieldData.h
@@ -9,19 +9,6 @@
 #define FIELDDATA_H
 
 
-//! An enumeration of the possible types (the different sub classes).
-enum FieldDataType
-{
-	typeFieldDataInt			= 0,
-	typeFieldDataFloat		= 1,
-	typeFieldDataDouble		= 2,
-	typeFieldDataString		= 3,
-	typeFieldDataBool			= 4
-};
-
-
-#include <iostream>
-using namespace std;
 
 
 //! This class is used as a front end to the standard data types.
@@ -29,75 +16,115 @@ using namespace std;
 	By using this class it is possible to place different data types in the same list. 
 */ 
 
+template<class data_type>
 class FieldData
 {
 
 
 	public:
 
-		//! The default constructor
+		//! The default constructor.
 		FieldData();
-		
-		//! The default destructor
+
+		//! Constructor which initializes the data.
+		FieldData( data_type data1 );		
+
+		//! The default destructor.
 		~FieldData();
 		
-		//! This function creates a new FieldDataInt object
-		FieldData* newFieldData( int 		value );
-		//! This function creates a new FieldDataFloat object
-		FieldData* newFieldData( float 	value );
-		//! This function creates a new FieldDataDouble object
-		FieldData* newFieldData( double value );
-		//! This function creates a new FieldDataString object
-		FieldData* newFieldData( string value );
-		//! This function creates a new FieldDataBool object
-		FieldData* newFieldData( bool 	value );
 
-		//! This function is implemented in FieldDataInt
-		virtual int getInt(){ cout << "Not sepose to happen Int\n"; };																						//TODO throw if not the correct data type
-		//! This function is implemented in FieldDataFloat
-		virtual float getFloat(){ cout << "Not sepose to happen Float\n"; };																			//TODO throw if not the correct data type
-		//! This function is implemented in FieldDataDouble
-		virtual double getDouble(){ cout << "Not sepose to happen Double\n"; };																	//TODO throw if not the correct data type
-		//! This function is implemented in FieldDataString
-		virtual string getString(){ cout << "Not sepose to happen String\n"; };																	//TODO throw if not the correct data type
-		//! This function is implemented in FieldDataBool
-		virtual bool getBool(){ cout << "Not sepose to happen Bool\n"; };																				//TODO throw if not the correct data type
+		//! Returns the the data from the current object.
+		data_type getData();
 		
-		//! This function is implemented in FieldDataInt
-		virtual void set( int 		value ){ cout << "Not sepose to happen Int\n"; };															//TODO throw if not the correct data type
-		//! This function is implemented in FieldDataFloat
-		virtual void set( float		value ){ cout << "Not sepose to happen Float\n"; };														//TODO throw if not the correct data type
-		//! This function is implemented in FieldDataDouble
-		virtual void set( double	value ){ cout << "Not sepose to happen Double\n"; };														//TODO throw if not the correct data type
-		//! This function is implemented in FieldDataString
-		virtual void set( string	value ){ cout << "Not sepose to happen String\n"; };														//TODO throw if not the correct data type
-		//! This function is implemented in FieldDataBool
-		virtual void set( bool		value ){ cout << "Not sepose to happen Bool\n"; };															//TODO throw if not the correct data type
+		//! Fuction to set the data.
+		void setData( data_type data1 );
+		
+		
 		
 		
 		//! Returns the type of subclass is used to store the data.
-		FieldDataType getType();
+		//FieldDataType getType();
 		
 		
 		//! Compare the value of two FieldData objects for equality
-		bool operator== ( FieldData* data );
+		bool operator== ( FieldData* data2 );
 		
 		//! Compare the value of two FieldData objects for inequality
-		bool operator!= ( FieldData* data );
-
-
-	protected:
-
-		FieldDataType type;
+		bool operator!= ( FieldData* data2 );
 
 
 	private:
 
-		
+		data_type data;
 
 };
 
 
+//-----------------------------------------------------------------------------
+
+
+template<class data_type>
+FieldData<data_type>::FieldData()
+{
+
+};
+
+//-----------------------------------------------------------------------------
+template<class data_type>
+FieldData<data_type>::FieldData( data_type data1 )
+{
+
+};
+	
+//-----------------------------------------------------------------------------
+
+template<class data_type>
+FieldData<data_type>::~FieldData()
+{
+
+};
+
+//-----------------------------------------------------------------------------
+
+template<class data_type>
+data_type FieldData<data_type>::getData()
+{
+
+};
+
+//-----------------------------------------------------------------------------
+
+template<class data_type>
+void FieldData<data_type>::setData( data_type data1 )
+{
+
+};
+
+//-----------------------------------------------------------------------------
+
+//template<class data_type>
+//FieldDataType FieldData<data_type>::getType()
+//{
+
+//};
+
+//-----------------------------------------------------------------------------
+
+template<class data_type>
+bool FieldData<data_type>::operator== ( FieldData* data2 )
+{
+
+};
+
+//-----------------------------------------------------------------------------
+
+template<class data_type>
+bool FieldData<data_type>::operator!= ( FieldData* data2 )
+{
+
+};
+
+//-----------------------------------------------------------------------------
 
 
 #endif
