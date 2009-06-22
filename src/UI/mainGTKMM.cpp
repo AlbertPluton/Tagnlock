50f8b713c5bfc51386687bbb0de8f4075292cf96
bart <bart@pluton.nl>
2009-06-22 13:44:16 +0000

    Backup before remodeling the GTKMM UI (forgot to add some files)

---
+++ src/UI/mainGTKMM.cpp
@@ -0,0 +1,20 @@
+//
+//	Author: A.L. Hertog
+//	Date: 9 January 2009
+//	License: GPL
+//
+//
+
+#include "EngineGTKMM.h"
+#include <string>
+using namespace std;
+
+
+int main (int argc, char **argv)
+{
+	string fileName = "src/UI/GTKMM/GTK_GUI.glade";
+	EngineGTKMM engine( argc, argv, fileName);
+
+
+
+}
