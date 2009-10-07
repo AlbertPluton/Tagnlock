ae2602f127b70ff711d53ce3ddaefef1ed7a7bf3
bart <bart@pluton.nl>
2009-10-07 13:00:53 +0000

    Added ToolchainWindowGTKMM

---
+++ src/UI/GTKMM/Toolchain/ToolchainWindowGTKMM.cpp
@@ -0,0 +1,10 @@
+//
+//	Author: A.L. Hertog
+//	Date: 7 October 2009
+//	License: GPL
+//
+//
+
+#include "ToolchainWindowGTKMM.h"
+
+
---
+++ src/UI/GTKMM/Toolchain/ToolchainWindowGTKMM.h
@@ -0,0 +1,46 @@
+//
+//	Author: A.L. Hertog
+//	Date: 7 October 2009
+//	License: GPL
+//
+//
+
+#ifndef TOOLCHAINWINDOWGTKMM
+#define TOOLCHAINWINDOWGTKMM
+
+
+
+#include "EngineGTKMM.h"
+
+#include <libglademm.h>
+#include <libglademm/xml.h>
+#include <gtkmm.h>
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+//! This class is an interface to the toolchain window and the engine. It is used to create new and alter exciting toolchains.
+
+class ToolchainWindowGTKMM : public EngineGTKMM
+{
+
+	public:
+		
+		//! Default constructor
+		ToolchainWindowGTKMM( int argc, char **argv, string gladeFileName );
+		
+		//! Default destructor
+		~ToolchainWindowGTKMM();
+
+	private
+
+
+
+};
+
+#endif
+
