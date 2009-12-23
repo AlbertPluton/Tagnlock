1df8e9e4b29f09964b3ea974dc654032a60fba02
bart <bart@pluton.nl>
2009-12-23 15:21:56 +0000

    added missing files to repository

---
+++ src/UI/dataGTKMM.cpp
@@ -0,0 +1,21 @@
+//
+//	Author: A.L. Hertog
+//	Date: 9 January 2009
+//	License: GPL
+//
+//
+
+#include "CategoryWindowGTKMM.h"
+#include "DataWindowGTKMM.h"
+#include "ToolchainWindowGTKMM.h"
+#include <string>
+using namespace std;
+
+
+int main (int argc, char **argv)
+{
+	string fileName = "src/UI/GTKMM/GTK_GUI.glade";
+
+	DataWindowGTKMM engine( argc, argv, fileName);
+
+};
